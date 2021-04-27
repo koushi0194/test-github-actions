96145feac10ee49c59e5d85bb707cca9069ec015
koushi0194 <koushi0194@gmail.com>
2021-04-27 17:48:28 +0530

    add test file with fastapi code

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print ("this is test file for git actions")
+import uvicorn
+import sys
+
+from fastapi import FastAPI
+
+version = f"{sys.version_info.major}.{sys.version_info.minor}"
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    message = f"Hello world! From FastAPI running on Uvicorn with Gunicorn. Using Python {version}"
+    return {"message": message}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000, host="0.0.0.0")
