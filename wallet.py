df6e8e207d2dd2b1f390e9157cfed669a7343d26
koushi0194 <koushi0194@gmail.com>
2021-04-27 23:01:07 +0530

    adding workflow & files

---
+++ wallet.py
@@ -0,0 +1,35 @@
+"""
+Wallet app
+"""
+
+class InsufficientAmount(Exception):
+    """
+    Insufficient Amount in wallet
+    """
+    pass
+
+
+class Wallet:
+    """
+    Wallet class to add & spend cash
+    """
+
+    def __init__(self, initial_amount=0):
+        """
+        Initial amount
+        """
+        self.balance = initial_amount
+
+    def spend_cash(self, amount):
+        """
+        Spend cash
+        """
+        if self.balance < amount:
+            raise InsufficientAmount('Not enough available to spend {}'.format(amount))
+        self.balance -= amount
+
+    def add_cash(self, amount):
+        """
+        Add cash to wallet
+        """
+        self.balance += amount
